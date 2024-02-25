dfce579c95be8ad790ce7ed96b0aeb26851d5bb6
Slava Zavgorodniy <slava.zavgorodniy@radcom.com>
2024-02-25 18:03:09 +0200

    add repositories
    - add DummyController
    - add method isRequestPermitted() into QuotaService

--- src/main/java/com/example/limiter/service/QuotaServiceImpl.java
+++ src/main/java/com/example/limiter/service/QuotaServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.limiter.service;
 
+import com.example.limiter.exception.LimiterException;
 import com.example.limiter.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,27 @@ public class QuotaServiceImpl implements QuotaService{
     @Override
     public Map<String, Integer> getUsersQuota() {
         return userDataService.getAllUsers();
+    }
+
+    @Override
+    public boolean isRequestPermitted(String userId) {
+        if(userId == null || userId.isBlank()){
+            throw new LimiterException("User id is required");
+        }
+        User user = userDataService.getUserById(userId);
+        if(user.isBlocked()){
+            throw new LimiterException("User is blocked");
+        }
+        var newQuota = user.getQuota() + 1;
+        if(newQuota > quota){
+           user.setBlocked(true);
+           user.setQuota(newQuota);
+           userDataService.updateUser(userId, user);
+              throw new LimiterException("Quota was reached. User is blocked");
+        }
+        user.setQuota(newQuota);
+        userDataService.updateUser(userId, user);
+        return true;
     }
 
 
